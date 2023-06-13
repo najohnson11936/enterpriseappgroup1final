6d2f5f10a3ad2b8997c1ae7519eac4fde32abe8b
Levi H <100445409+lh1207@users.noreply.github.com>
2023-06-13 18:04:14 -0400

    initialized project with spring web and thymeleaf

---
+++ src/main/java/com/vehicoolrentals/app/VehicoolRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.vehicoolrentals.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicoolRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicoolRentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vehicoolrentals/app/VehicoolRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicoolrentals.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicoolRentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
